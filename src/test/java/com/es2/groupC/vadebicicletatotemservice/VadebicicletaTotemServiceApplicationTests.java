7ecd8686a81a1fdc2c875bfcb8876e3cb33878d9
fabriciobcv <fabriciobcvpessoal@gmail.com>
2024-04-14 17:37:15 -0300

    commit inicial

---
+++ src/test/java/com/es2/groupC/vadebicicletatotemservice/VadebicicletaTotemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es2.groupC.vadebicicletatotemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VadebicicletaTotemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
