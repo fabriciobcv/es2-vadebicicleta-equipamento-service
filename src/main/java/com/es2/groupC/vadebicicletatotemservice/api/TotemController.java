c47fa6b88be6d71bd0240ad2bc68c87886c16c44
Celio Camara <celiofcjunior_cg@hotmail.com>
2024-04-14 19:49:13 -0300

    Refatorado endpoints

--- src/main/java/com/es2/groupC/vadebicicletatotemservice/api/TotemController.java
+++ src/main/java/com/es2/groupC/vadebicicletatotemservice/api/TotemController.java
@@ -9,24 +9,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/operacao")
+@RequestMapping("/totens")
 public class TotemController {
-    @PostMapping("/retirar-da-tranca")
-    public ResponseEntity<RespostaPadrao> retirarBicicleta(@RequestBody RetirarDaTrancaDTO dto){
+    @PatchMapping("/{numeroTotem}/retirar-da-tranca")
+    public ResponseEntity<RespostaPadrao> retirarBicicleta(@PathVariable String numeroTotem, @RequestBody RetirarDaTrancaDTO dto){
         return ResponseEntity.ok(new RespostaPadrao());
     }
 
-    @PostMapping("/colocar-na-tranca")
-    public ResponseEntity<RespostaPadrao> colocarBicicleta(@RequestBody ColocarNaTrancaDTO dto){
+    @PatchMapping("/{numeroTotem}/colocar-na-tranca")
+    public ResponseEntity<RespostaPadrao> colocarBicicleta(@PathVariable String numeroTotem, @RequestBody ColocarNaTrancaDTO dto){
         return ResponseEntity.ok(new RespostaPadrao());
     }
 
-    @PostMapping("/totens/{numeroTotem}/incluir-tranca")
+    @PatchMapping("/totens/{numeroTotem}/incluir-tranca")
     public ResponseEntity<RespostaPadrao> incluirTranca(@PathVariable String numeroTotem, @RequestBody IncluirTrancaDTO dto){
         return ResponseEntity.ok(new RespostaPadrao());
     }
 
-    @PostMapping("/totens/{numeroTotem}/retirar-tranca")
+    @PatchMapping("/totens/{numeroTotem}/retirar-tranca")
     public ResponseEntity<RespostaPadrao> retirarTranca(@PathVariable String numeroTotem, @RequestBody RetirarTrancaDTO dto){
         return ResponseEntity.ok(new RespostaPadrao());
     }
